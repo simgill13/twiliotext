5b91b65b3cf68d9dd67f27475968af63e096b4ce
Your Name <sim@Sims-MacBook-Pro.local>
2017-09-10 19:21:48 -0700

    test

---
+++ server.js
@@ -0,0 +1,108 @@
+ const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+const {bar,order} = require('./model');
+const {PORT, DATABASE_URL} = require('./config');
+
+
+
+
+
+
+
+
+app.post('/textmsg', (req, res) => {
+ console.log('=================HIT==============')
+   res.status(200).json({msg: 'this is working'})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let server;
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
